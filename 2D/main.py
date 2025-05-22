0801ae3ee7388dd129809ff62d08b288871ea4a3
SergioFrasco <sergio.frascoza@gmail.com>
2025-05-22 09:58:37 +0200

    Massive run

--- 2D/main.py
+++ 2D/main.py
@@ -34,7 +34,7 @@ sys.path.append(".")
 # epsilon decay = 0.995 before
 # 0.999 better
 
-def train_successor_agent(agent, env, episodes = 4001, ae_model=None, max_steps=100, epsilon_start=1.0, epsilon_end=0.05, epsilon_decay=0.995, train_vision_threshold=0.1):
+def train_successor_agent(agent, env, episodes = 20001, ae_model=None, max_steps=100, epsilon_start=1.0, epsilon_end=0.05, epsilon_decay=0.995, train_vision_threshold=0.1):
     """
     Training loop for SuccessorAgent in MiniGrid environment with vision model integration, SR tracking, and WVF formation
     """
