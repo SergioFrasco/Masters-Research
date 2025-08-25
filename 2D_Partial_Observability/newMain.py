4ea9a1140191f6061e684c7f1c0a1ff638bcae89
SergioFrasco <sergio.frascoza@gmail.com>
2025-08-25 09:18:55 +0200

    run on cluster

--- 2D_Partial_Observability/newMain.py
+++ 2D_Partial_Observability/newMain.py
@@ -1068,7 +1068,7 @@ def main():
     runner = ExperimentRunner(env_size=10, num_seeds=1)
 
     # Run experiments
-    results = runner.run_comparison_experiment(episodes=1000)
+    results = runner.run_comparison_experiment(episodes=5000)
 
     # Analyze and plot results
     summary = runner.analyze_results(window=100)
