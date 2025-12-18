852f3a73037877d8e6d0af83d403a9f2aeccd7bb
SergioFrasco <sergio.frascoza@gmail.com>
2025-12-18 17:40:08 +0200

    memory fix

--- 3D_Composition/run_experiments.py
+++ 3D_Composition/run_experiments.py
@@ -53,8 +53,8 @@ ALGORITHMS = ["SR", "DQN", "LSTM", "WVF"]
 
 # Training configuration
 TRAINING_CONFIG = {
-    "training_episodes": 10,
-    "eval_episodes_per_task": 10,  # 300 per compositional task
+    "training_episodes": 10000,
+    "eval_episodes_per_task": 300,  # 300 per compositional task
     "max_steps": 200,
     "env_size": 10,
     
@@ -74,8 +74,7 @@ SLURM_CONFIG = {
     "partition": "bigbatch",
     "time": 72 * 60,  # 72 hours in minutes
     "ntasks": 1,
-    "cpus_per_task": 1,
-    "mem": "8GB",
+    # Note: mem and cpus_per_task removed - cluster doesn't allow these specifications
 }
 
 # Output directory structure
